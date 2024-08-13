783dc07577d9b279f868f9eb036dc768cbcbaa2f
Kristina-Uryevna <mironovakristina.ur@gmail.com>
2024-08-13 12:16:58 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Kristina"
+print("Name: " + name)
+age = 32
+print("Age: ", age)
+new_age = age + 1
+print("New_age: ", new_age)
+name = age != new_age
+is_student = name
+print("Is_student: ", is_student)
